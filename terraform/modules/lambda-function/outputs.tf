7f7d78d87d97dedd4ef213c173a7f2cb87a90f15
Minh Luan <phamminhluan@fabercompany.co.jp>
2026-02-06 10:50:54 +0700

    Update MonthlyAddingSiteTablesConsumer function name and remove unused output variables
    Signed-off-by: Minh Luan <phamminhluan@fabercompany.co.jp>

--- terraform/modules/lambda-function/outputs.tf
+++ terraform/modules/lambda-function/outputs.tf
@@ -7,13 +7,3 @@ output "function_arn" {
   description = "Lambda function ARN"
   value       = aws_lambda_function.this.arn
 }
-
-output "function_invoke_arn" {
-  description = "Lambda function invoke ARN"
-  value       = aws_lambda_function.this.invoke_arn
-}
-
-output "security_group_id" {
-  description = "Security group ID (if created)"
-  value       = try(aws_security_group.lambda[0].id, null)
-}
